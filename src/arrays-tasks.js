d77b2a33fd073c87b8cc92c1d0202c14905fdaa2
turik777 <52492191+turik777@users.noreply.github.com>
2024-12-04 04:32:45 +0300

    feat: complete getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -127,8 +127,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length > 0
+    ? parseFloat(
+        (arr.reduce((sum, item) => sum + item, 0) / arr.length).toFixed(2)
+      )
+    : 0;
 }
 
 /**
