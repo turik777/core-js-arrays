dc5bfbd410130f8240ab97b348cd48de9633d984
turik777 <52492191+turik777@users.noreply.github.com>
2024-12-04 03:41:07 +0300

    feat: complete getIdentityMatrix function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -403,8 +403,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (row, i) =>
+    Array.from({ length: n }, (value, j) => (i === j ? 1 : 0))
+  );
 }
 
 /**
