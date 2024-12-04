86779b478e2ef4ff331c691a11b49f42af031719
turik777 <52492191+turik777@users.noreply.github.com>
2024-12-04 03:45:14 +0300

    feat: complete getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -420,8 +420,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce(
+    (odd, element, index) => (element % 2 === 1 ? [...odd, index] : odd),
+    []
+  );
 }
 
 /**
